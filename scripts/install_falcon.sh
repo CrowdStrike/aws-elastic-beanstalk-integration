281f531ece3d54232969aafa145c766a849e4bcc
Gax <gax.theodorio@crowdstrike.com>
2025-01-08 23:48:45 -0500

    Pull Falcon install script from main

--- scripts/install_falcon.sh
+++ scripts/install_falcon.sh
@@ -3,4 +3,4 @@
 export FALCON_CLIENT_ID="$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --query SecretString --output text | jq -r .FALCON_CLIENT_ID)"
 export FALCON_CLIENT_SECRET="$(aws secretsmanager get-secret-value --secret-id "$SECRET_ID" --query SecretString --output text | jq -r .FALCON_CLIENT_SECRET)"
 
-curl -L https://raw.githubusercontent.com/crowdstrike/falcon-scripts/v1.7.1/bash/install/falcon-linux-install.sh | bash
+curl -L https://raw.githubusercontent.com/crowdstrike/falcon-scripts/main/bash/install/falcon-linux-install.sh | bash
